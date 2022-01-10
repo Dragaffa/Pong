a4e4d169aaba546ba1e3a2426ecd0f058c981795
Dragaffa <noa.affagard@gmail.com>
2022-01-10 15:57:33 +0100

    4

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -15,6 +15,7 @@ class Tableau1 extends Phaser.Scene{
 
     create(){
 
+        this.initKeyboard()
         this.hauteur=500
         this.largeur=1000
 
@@ -25,18 +26,36 @@ class Tableau1 extends Phaser.Scene{
         this.ball.setMaxVelocity(Phaser.Math.Between(200,-200));
         //this.ball.setMaxVelocity(1000,1000);
 
-        this.haut=this.physics.add.sprite(0,0,'carre').setOrigin(0,0);
+        this.haut=this.physics.add.sprite(0,-20,'carre').setOrigin(0,0);
         this.haut.setDisplaySize(this.largeur,20);
         this.haut.body.setAllowGravity(false);
         this.haut.setImmovable(true);
 
-        this.bas=this.physics.add.sprite(0,this.hauteur-20, 'carre').setOrigin(0,0);
+        this.bas=this.physics.add.sprite(0,this.hauteur, 'carre').setOrigin(0,0);
         this.bas.setDisplaySize(this.largeur,20);
         this.bas.body.setAllowGravity(false);
         this.bas.setImmovable(true);
 
+        this.rat1=this.physics.add.sprite(this.largeur-20,this.hauteur/2, 'carre')
+        this.rat1.setDisplaySize(20,100);
+        this.rat1.body.setAllowGravity(false);
+        this.rat1.setImmovable(true);
+
+        this.rat2=this.physics.add.sprite(20,this.hauteur/2, 'carre')
+        this.rat2.setDisplaySize(20,100);
+        this.rat2.body.setAllowGravity(false);
+        this.rat2.setImmovable(true);
+
         this.physics.add.collider(this.ball,this.bas);
         this.physics.add.collider(this.ball,this.haut);
+        this.physics.add.collider(this.ball,this.rat1);
+        this.physics.add.collider(this.ball,this.rat2);
+    }
+
+    initKeyboard(){
+        this.input.keyboard.on('keydown', function (kevent) {
+            il faut utiliser la  velocity pour le moi du futur
+        }
     }
 
     update(){
